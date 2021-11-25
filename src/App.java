e3d5015405ae1ae2781661294854dd260d1b6e4f
FranCILDO Alves <53942335+francildoalves@users.noreply.github.com>
2021-11-25 15:31:55 -0300

    Atualização das classes

---
+++ src/App.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author FranCILDO Alves
+ * 
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Funcionando");
+    }
+    
+}
+
+
---
+++ src/ContaBanco.java
@@ -0,0 +1,35 @@
+/** 
+ * 
+ * A classe ContaBanco contém os atributos e métodos para manipular as solicitações de uma conta bancároa.
+ * 
+ * @param numConta - Armazena um número inteiro com o número da conta
+ * @param tipo -  Armazena uma String com o tipo de conta que será criada que pode ser uma Conta Corrente "CC" ou Conta Poupança.
+ * @param dono -  Armazena uma String com o nome do dono da conta.
+ * @param saldo - Armazena um múmero do tipo float com o saldo atual da conta.
+ * @param status - Armazena um valor booleano com o status da conta, sendo true para conta aberta e false para conta fechada.
+ * @author FranCILDO Alves
+ * @date 25/11/2021
+ */
+public class ContaBanco {
+    private int numConta;
+    private String tipo;
+    private String dono;
+    private float saldo;
+
+    
+
+    /**
+     * O método AbriConta é responsável por abrir uma conta    */
+    
+    private abrirConta(){}
+
+    private fecharConta(){}
+
+    private depositar(){}
+
+    sacar(){}
+
+    pagarMensal(){}
+
+
+}
